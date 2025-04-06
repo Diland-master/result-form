57e9e368098ad3c7288fa5340e23115fefc163d7
Earthlings-online <earthlings-online@ya.ru>
2025-04-06 21:26:15 +0500

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import { useState, useRef, useEffect } from 'react'
 import styles from './app.module.css'
 import { Field } from './components'
-
-const validateFields = (name, value, password = null) => {
-	switch (name) {
-		case 'email':
-			if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) return 'Некорректный email'
-			return null
-		case 'password':
-			if (value.length < 8) return 'Пароль должен быть не менее 8 символов'
-			if (value.length > 32) return 'Пароль должен быть не более 32 символов'
-			if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/.test(value)) return 'Пароль должен содержать заглавную букву, строчную букву и цифру'
-			return null
-		case 'passwordConfirm':
-			if (value !== password) return 'Пароли не совпадают'
-			return null
-		default:
-			return null
-	}
-}
+import { validateFields } from './validation/validateField'
 
 export const App = () => {
 	const [formData, setFormData] = useState({
@@ -82,7 +65,7 @@ export const App = () => {
 
 			<Field
 				name='password'
-				type='text'
+				type='password'
 				placeholder='Пароль'
 				value={formData.password}
 				onChange={onChange}
@@ -93,7 +76,7 @@ export const App = () => {
 
 			<Field
 				name='passwordConfirm'
-				type='text'
+				type='password'
 				placeholder='Повторите пароль'
 				value={formData.passwordConfirm}
 				onChange={onChange}
