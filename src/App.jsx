46dcdcbffa1e97a3025008b1cfb579edd961621a
Earthlings-online <earthlings-online@ya.ru>
2025-04-06 21:19:55 +0500

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,88 +2,107 @@ import { useState, useRef, useEffect } from 'react'
 import styles from './app.module.css'
 import { Field } from './components'
 
+const validateFields = (name, value, password = null) => {
+	switch (name) {
+		case 'email':
+			if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) return 'Некорректный email'
+			return null
+		case 'password':
+			if (value.length < 8) return 'Пароль должен быть не менее 8 символов'
+			if (value.length > 32) return 'Пароль должен быть не более 32 символов'
+			if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/.test(value)) return 'Пароль должен содержать заглавную букву, строчную букву и цифру'
+			return null
+		case 'passwordConfirm':
+			if (value !== password) return 'Пароли не совпадают'
+			return null
+		default:
+			return null
+	}
+}
+
 export const App = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [passwordConfirm, setPasswordСonfirm] = useState('')
-	const [emailError, setEmailError] = useState(null)
-	const [passwordError, setPasswordError] = useState(null)
-	const [passwordConfirmError, setPasswordСonfirmError] = useState(null)
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+		passwordConfirm: '',
+	})
+	const [errors, setErrors] = useState({
+		email: null,
+		password: null,
+		passwordConfirm: null,
+	})
 
 	const submitButtonRef = useRef(null)
 
-	const onEmailChange = ({ target }) => {
-		setEmail(target.value)
-		setEmailError(null)
+	const isFieldValid = Object.values(errors).some((value) => value !== null)
+	const isFormValid = Object.keys(formData).every((field) => {
+		const error = validateFields(field, formData[field], formData.password)
+		return error === null
+	})
+
+	const onChange = ({ target }) => {
+		const { name, value } = target
+
+		setFormData({ ...formData, [name]: value })
+		setErrors({ ...errors, [name]: null })
 	}
 
-	const onEmailBlur = ({ target }) => {
-		if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(target.value)) {
-			setEmailError('Некорректный email')
+	const onBlur = ({ target }) => {
+		const { name, value } = target
+
+		setErrors({ ...errors, [name]: validateFields(name, value, formData.password) })
+	}
+
+	useEffect(() => {
+		if (isFormValid) {
+			submitButtonRef.current?.focus()
 		}
-	}
-
-	const onPasswordChange = ({ target }) => {
-		setPassword(target.value)
-		setPasswordError(null)
-
-		if (target.value.length > 32) {
-			setPasswordError('Пароль должен быть не более 32 символов')
-		}
-	}
-
-	const onPasswordBlur = ({ target }) => {
-		if (target.value.length < 8) {
-			setPasswordError('Пароль должен быть не менее 8 символов')
-		} else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/.test(target.value)) {
-			setPasswordError('Пароль должен содержать заглавную букву, строчную букву и цифру')
-		}
-	}
-
-	const onPasswordСonfirmChange = ({ target }) => {
-		setPasswordСonfirm(target.value)
-		setPasswordСonfirmError(null)
-	}
-
-	const onPasswordСonfirmBlur = () => {
-		if (passwordConfirm !== password) {
-			setPasswordСonfirmError('Пароли не совпадают')
-		}
-	}
+	}, [isFormValid])
 
 	const onSubmit = (event) => {
 		event.preventDefault()
 
-		console.log({ email, password, passwordConfirm })
+		if (!isFormValid) return
+
+		console.log(formData)
 	}
 
 	return (
 		<form onSubmit={onSubmit} className={styles.form}>
-			<Field name='email' type='email' placeholder='E-mail' value={email} onChange={onEmailChange} onBlur={onEmailBlur} error={emailError} required />
+			<Field
+				name='email'
+				type='email'
+				placeholder='E-mail'
+				value={formData.email}
+				onChange={onChange}
+				onBlur={onBlur}
+				error={errors.email}
+				required
+			/>
 
 			<Field
 				name='password'
-				type='password'
+				type='text'
 				placeholder='Пароль'
-				value={password}
-				onChange={onPasswordChange}
-				onBlur={onPasswordBlur}
-				error={passwordError}
+				value={formData.password}
+				onChange={onChange}
+				onBlur={onBlur}
+				error={errors.password}
 				required
 			/>
 
 			<Field
 				name='passwordConfirm'
-				type='password'
+				type='text'
 				placeholder='Повторите пароль'
-				value={passwordConfirm}
-				onChange={onPasswordСonfirmChange}
-				onBlur={onPasswordСonfirmBlur}
-				error={passwordConfirmError}
+				value={formData.passwordConfirm}
+				onChange={onChange}
+				onBlur={onBlur}
+				error={errors.passwordConfirm}
 				required
 			/>
 
-			<button ref={submitButtonRef} type='submit' className={styles.button} disabled={!!passwordError || !!emailError || !!passwordConfirmError}>
+			<button ref={submitButtonRef} type='submit' className={styles.button} disabled={isFieldValid}>
 				Зарегистрироваться
 			</button>
 		</form>
